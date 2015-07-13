8d62a9ce1247b1a52702e2dc8385acf9c852ba8a
Martin Treacy-Schwartz <thetreacyschwartz@gmail.com>
2015-07-13 10:35:57 +0200

    Update to v2.1.0

--- GameAnalytics/Plugins/Playmaker/SendBusinessEvent.cs
+++ GameAnalytics/Plugins/Playmaker/SendBusinessEvent.cs
@@ -6,64 +6,13 @@ using GameAnalyticsSDK;
 namespace HutongGames.PlayMaker.Actions
 {
 	[ActionCategory("GameAnalytics")]
-	[Tooltip("Sends a business event message to the GameAnalytics server")]
+	[Tooltip("Sends a business event message to the GameAnalytics server with no validation")]
 	[HelpUrl("https://hutonggames.fogbugz.com/default.asp?W1163")]
 	public class SendBusinessEvent : FsmStateAction
 	{
 		[RequiredField]
 		[Tooltip("Abbreviation of the currency used for the transaction. F.x. USD (U.S. Dollars)")]
 		public FsmString Currency;
-		
-		[RequiredField]
-		[Tooltip("Amount of real currency, in cents")]
-		public FsmInt Amount;
-
-		[RequiredField]
-		[Tooltip("Type of IAP item purchased (e.g. Coins)")]
-		public FsmString ItemType;
-		
-		[RequiredField]
-		[Tooltip("Specific item purchased (e.g. CoinPack001)")]
-		public FsmString ItemID;
-
-		[RequiredField]
-		[Tooltip("Cart Type")]
-		public FsmString CartType;
-
-		[Tooltip("App Store Receipt, used for purchase validation")]
-		public FsmString Receipt;
-		
-		[RequiredField]
-		[Tooltip("If true the SDK will ignore the receipt parameter and attempt to automatically get the receipt.")]
-		public FsmBool AutoFetchReceipt;
-		
-		public override void Reset()
-		{
-			Currency = new FsmString() { UseVariable = false };
-			Amount = new FsmInt() { UseVariable = false };
-			ItemType = new FsmString() { UseVariable = false };
-			ItemID = new FsmString() { UseVariable = false };
-			CartType = new FsmString() { UseVariable = false };
-			Receipt = new FsmString() { UseVariable = false };
-			AutoFetchReceipt = new FsmBool() { UseVariable = false };
-		}
-		
-		public override void OnEnter()
-		{
-			GameAnalytics.NewBusinessEvent(Currency.Value, Amount.Value, ItemType.Value, ItemID.Value, CartType.Value, Receipt.Value, AutoFetchReceipt.Value);
-			
-			Finish();
-		}
-	}
-
-	[ActionCategory("GameAnalytics")]
-	[Tooltip("Sends an iOS business event message to the GameAnalytics server")]
-	[HelpUrl("https://hutonggames.fogbugz.com/default.asp?W1163")]
-	public class SendBusinessEventIOS : FsmStateAction
-	{
-		[RequiredField]
-		[Tooltip("Abbreviation of the currency used for the transaction. F.x. USD (U.S. Dollars)")]
-		public FsmString Currency;
 
 		[RequiredField]
 		[Tooltip("Amount of real currency, in cents")]
@@ -81,13 +30,6 @@ namespace HutongGames.PlayMaker.Actions
 		[Tooltip("Cart Type")]
 		public FsmString CartType;
 
-		[Tooltip("App Store Receipt, used for purchase validation")]
-		public FsmString Receipt;
-
-		[RequiredField]
-		[Tooltip("If true the SDK will ignore the receipt parameter and attempt to automatically get the receipt.")]
-		public FsmBool AutoFetchReceipt;
-
 		public override void Reset()
 		{
 			Currency = new FsmString() { UseVariable = false };
@@ -95,18 +37,107 @@ namespace HutongGames.PlayMaker.Actions
 			ItemType = new FsmString() { UseVariable = false };
 			ItemID = new FsmString() { UseVariable = false };
 			CartType = new FsmString() { UseVariable = false };
-			Receipt = new FsmString() { UseVariable = false };
-			AutoFetchReceipt = new FsmBool() { UseVariable = false };
 		}
 
 		public override void OnEnter()
 		{
-			GA_Business.NewEventIOS(Currency.Value, Amount.Value, ItemType.Value, ItemID.Value, CartType.Value, Receipt.Value, AutoFetchReceipt.Value);
+			GameAnalytics.NewBusinessEvent(Currency.Value, Amount.Value, ItemType.Value, ItemID.Value, CartType.Value);
 
 			Finish();
 		}
 	}
 
+#if UNITY_IOS
+[ActionCategory("GameAnalytics")]
+[Tooltip("Sends an iOS business event message to the GameAnalytics server")]
+[HelpUrl("https://hutonggames.fogbugz.com/default.asp?W1163")]
+public class SendBusinessEventIOS : FsmStateAction
+{
+[RequiredField]
+[Tooltip("Abbreviation of the currency used for the transaction. F.x. USD (U.S. Dollars)")]
+public FsmString Currency;
+
+[RequiredField]
+[Tooltip("Amount of real currency, in cents")]
+public FsmInt Amount;
+
+[RequiredField]
+[Tooltip("Type of IAP item purchased (e.g. Coins)")]
+public FsmString ItemType;
+
+[RequiredField]
+[Tooltip("Specific item purchased (e.g. CoinPack001)")]
+public FsmString ItemID;
+
+[RequiredField]
+[Tooltip("Cart Type")]
+public FsmString CartType;
+
+[Tooltip("App Store Receipt, used for purchase validation")]
+public FsmString Receipt;
+
+public override void Reset()
+{
+Currency = new FsmString() { UseVariable = false };
+Amount = new FsmInt() { UseVariable = false };
+ItemType = new FsmString() { UseVariable = false };
+ItemID = new FsmString() { UseVariable = false };
+CartType = new FsmString() { UseVariable = false };
+Receipt = new FsmString() { UseVariable = false };
+}
+
+public override void OnEnter()
+{
+GameAnalytics.NewBusinessEventIOS(Currency.Value, Amount.Value, ItemType.Value, ItemID.Value, CartType.Value, Receipt.Value);
+
+Finish();
+}
+}
+
+[ActionCategory("GameAnalytics")]
+[Tooltip("Sends an iOS business with auto fetch receipt event message to the GameAnalytics server")]
+[HelpUrl("https://hutonggames.fogbugz.com/default.asp?W1163")]
+public class SendBusinessEventIOSAutoFetchReceipt : FsmStateAction
+{
+[RequiredField]
+[Tooltip("Abbreviation of the currency used for the transaction. F.x. USD (U.S. Dollars)")]
+public FsmString Currency;
+
+[RequiredField]
+[Tooltip("Amount of real currency, in cents")]
+public FsmInt Amount;
+
+[RequiredField]
+[Tooltip("Type of IAP item purchased (e.g. Coins)")]
+public FsmString ItemType;
+
+[RequiredField]
+[Tooltip("Specific item purchased (e.g. CoinPack001)")]
+public FsmString ItemID;
+
+[RequiredField]
+[Tooltip("Cart Type")]
+public FsmString CartType;
+
+public override void Reset()
+{
+Currency = new FsmString() { UseVariable = false };
+Amount = new FsmInt() { UseVariable = false };
+ItemType = new FsmString() { UseVariable = false };
+ItemID = new FsmString() { UseVariable = false };
+CartType = new FsmString() { UseVariable = false };
+}
+
+public override void OnEnter()
+{
+GameAnalytics.NewBusinessEventIOSAutoFetchReceipt(Currency.Value, Amount.Value, ItemType.Value, ItemID.Value, CartType.Value);
+
+Finish();
+}
+}
+#endif
+
+#if UNITY_ANDROID
 	[ActionCategory("GameAnalytics")]
 	[Tooltip("Sends a Google Play business event message to the GameAnalytics server")]
 	[HelpUrl("https://hutonggames.fogbugz.com/default.asp?W1163")]
@@ -157,6 +188,7 @@ namespace HutongGames.PlayMaker.Actions
 			Finish();
 		}
 	}
+#endif
 }
 
 #endif
