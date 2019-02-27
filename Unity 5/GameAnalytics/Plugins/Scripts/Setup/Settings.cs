6bfef23b2c5bcc56254b94f853f8262ffecd9d29
Martin Treacy-Schwartz <mts@gameanalytics.com>
2019-02-27 09:59:17 +0100

    Updated to v5.1.5

--- Unity 5/GameAnalytics/Plugins/Scripts/Setup/Settings.cs
+++ Unity 5/GameAnalytics/Plugins/Scripts/Setup/Settings.cs
@@ -53,7 +53,7 @@ namespace GameAnalyticsSDK.Setup
         /// The version of the GA Unity Wrapper plugin
         /// </summary>
         [HideInInspector]
-        public static string VERSION = "5.1.4";
+        public static string VERSION = "5.1.5";
 
         [HideInInspector]
         public static bool CheckingForUpdates = false;
