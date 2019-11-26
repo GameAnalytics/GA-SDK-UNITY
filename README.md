8b00c4b60eaaeaac455b813dc2854f1be8c648ab
Martin Treacy-Schwartz <mts@gameanalytics.com>
2019-11-26 10:31:43 +0100

    Updated to v6.0.4

--- README.md
+++ README.md
@@ -21,6 +21,9 @@ If you have any issues or feedback regarding the SDK, please contact our friendl
 Changelog
 ---------
 <!--(CHANGELOG_TOP)-->
+**6.0.4**
+* fixed ios events bug
+
 **6.0.3**
 * fixed ios build error
 
--- Unity 4/GameAnalytics/Plugins/Scripts/Setup/Settings.cs
+++ Unity 4/GameAnalytics/Plugins/Scripts/Setup/Settings.cs
@@ -51,7 +51,7 @@ namespace GameAnalyticsSDK.Setup
         /// The version of the GA Unity Wrapper plugin
         /// </summary>
         [HideInInspector]
-        public static string VERSION = "6.0.3";
+        public static string VERSION = "6.0.4";
 
         [HideInInspector]
         public static bool CheckingForUpdates = false;
