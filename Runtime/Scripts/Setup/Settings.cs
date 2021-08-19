4fed24e4e7d50fee54697d223e781cd4ceb92266
Martin Treacy-Schwartz <mts@gameanalytics.com>
2021-08-19 12:58:04 +0200

    Updated to v7.0.0

--- Runtime/Scripts/Setup/Settings.cs
+++ Runtime/Scripts/Setup/Settings.cs
@@ -53,7 +53,7 @@ namespace GameAnalyticsSDK.Setup
         /// The version of the GA Unity Wrapper plugin
         /// </summary>
         [HideInInspector]
-        public static string VERSION = "6.7.1";
+        public static string VERSION = "7.0.0";
 
         [HideInInspector]
         public static bool CheckingForUpdates = false;
