f006ae6f0b3d35dd330b9a222125a9e7653c9f89
Dorin <dorin@gameanalytics.com>
2022-12-07 10:36:13 +0200

    Updated to v7.5.0

--- Runtime/Scripts/Setup/Settings.cs
+++ Runtime/Scripts/Setup/Settings.cs
@@ -53,7 +53,7 @@ namespace GameAnalyticsSDK.Setup
         /// The version of the GA Unity Wrapper plugin
         /// </summary>
         [HideInInspector]
-        public static string VERSION = "7.4.4";
+        public static string VERSION = "7.5.0";
 
         [HideInInspector]
         public static bool CheckingForUpdates = false;
