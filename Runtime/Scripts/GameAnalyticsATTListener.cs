09b46fff89821792bb5f6f0adc8bfa2798e06978
CaseyHofland <hofland.casey@gmail.com>
2023-10-19 18:38:50 +0200

    Add default ATT listener onto the GameAnalytics prefab
    The default ATT Listener will handle requesting tracking authorization in case of playing on an IPhone, and initializing normally in all other cases.
    
    This default ATT listener provides an easy way to initialize game analytics. Additionally it has been added to the Game Analytics default prefab.

---
+++ Runtime/Scripts/GameAnalyticsATTListener.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using GameAnalyticsSDK;
+
+public class GameAnalyticsATTListener : MonoBehaviour, IGameAnalyticsATTListener
+{
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            GameAnalytics.RequestTrackingAuthorization(this);
+        }
+        else
+        {
+            GameAnalytics.Initialize();
+        }
+    }
+
+    public void GameAnalyticsATTListenerNotDetermined()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerRestricted()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerDenied()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerAuthorized()
+    {
+        GameAnalytics.Initialize();
+    }
+}
