1e1d9e993b4a23012fc83767d700b858cea04283
CaseyHofland <hofland.casey@gmail.com>
2023-10-26 18:07:28 +0200

    Put the Listener under the GASDK namespace

--- Runtime/Scripts/GameAnalyticsATTListener.cs
+++ Runtime/Scripts/GameAnalyticsATTListener.cs
@@ -1,34 +1,36 @@
 using UnityEngine;
-using GameAnalyticsSDK;
 
-public class GameAnalyticsATTListener : MonoBehaviour, IGameAnalyticsATTListener
+namespace GameAnalyticsSDK
 {
-    void Start()
+    public class GameAnalyticsATTListener : MonoBehaviour, IGameAnalyticsATTListener
     {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        void Start()
         {
-            GameAnalytics.RequestTrackingAuthorization(this);
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                GameAnalytics.RequestTrackingAuthorization(this);
+            }
+            else
+            {
+                GameAnalytics.Initialize();
+            }
         }
-        else
+
+        public void GameAnalyticsATTListenerNotDetermined()
         {
             GameAnalytics.Initialize();
         }
-    }
-
-    public void GameAnalyticsATTListenerNotDetermined()
-    {
-        GameAnalytics.Initialize();
-    }
-    public void GameAnalyticsATTListenerRestricted()
-    {
-        GameAnalytics.Initialize();
-    }
-    public void GameAnalyticsATTListenerDenied()
-    {
-        GameAnalytics.Initialize();
-    }
-    public void GameAnalyticsATTListenerAuthorized()
-    {
-        GameAnalytics.Initialize();
+        public void GameAnalyticsATTListenerRestricted()
+        {
+            GameAnalytics.Initialize();
+        }
+        public void GameAnalyticsATTListenerDenied()
+        {
+            GameAnalytics.Initialize();
+        }
+        public void GameAnalyticsATTListenerAuthorized()
+        {
+            GameAnalytics.Initialize();
+        }
     }
 }
